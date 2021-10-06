432182f039e48ee66b6e215be127bf735c28e54c
Alex Rado <alexaccandr@gmail.com>
2021-10-06 09:48:46 +0300

    First

--- app/src/main/java/com/github/takahirom/dagger/hilt/multimodule/sample/ApplicationModule.kt
+++ app/src/main/java/com/github/takahirom/dagger/hilt/multimodule/sample/ApplicationModule.kt
@@ -5,9 +5,9 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ApplicationComponent
-
 @Module
 @InstallIn(ApplicationComponent::class)
+
 class ApplicationModule {
 
   @Provides
